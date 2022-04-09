5a6ddc5465a9e757c02f2759471a7611a7f0ae02
EC2 Default User <ec2-user@ip-172-31-90-125.ec2.internal>
2022-04-09 11:29:41 +0000

    [Update]books

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'homes/index'
+  root :to => 'homes#top'
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
