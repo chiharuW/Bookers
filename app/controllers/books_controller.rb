97bc8d5969a948fb25150418b9f4009099511eaa
EC2 Default User <ec2-user@ip-172-31-90-125.ec2.internal>
2022-04-15 13:09:18 +0000

    [Update]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,14 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+   if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+   else
+      flash[:alret] = "投稿に失敗しました。"
+      @books = Book.all
+      render "index"
+   end
   end
 
   def show
